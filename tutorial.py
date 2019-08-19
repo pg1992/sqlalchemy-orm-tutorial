5a33dd632e06d29273f4f771177a5dbac2c9d3d8
Pedro Guilherme Siqueira Moreira <pedro.guilherme@idvlab.com.br>
2019-08-19 17:42:06 -0300

    Connect to an in memory SQLite database

--- tutorial.py
+++ tutorial.py
@@ -5,8 +5,17 @@ def sqlalchemy_version():
     print('SQLAlchemy version: {}'.format(sql.__version__))
 
 
+def create_engine():
+    eng = sql.create_engine('sqlite:///:memory:', echo=True)
+
+    print(eng)
+
+    return eng
+
+
 def main():
     sqlalchemy_version()
+    eng = create_engine()
 
 
 if __name__ == '__main__':
