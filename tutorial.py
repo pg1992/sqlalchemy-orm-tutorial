82c845b374a38a98b63fcf96758c29581e50392b
Pedro Guilherme Siqueira Moreira <pedro.guilherme@idvlab.com.br>
2019-08-20 11:12:48 -0300

    Declare a mapping

--- tutorial.py
+++ tutorial.py
@@ -1,4 +1,22 @@
 import sqlalchemy as sql
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = sql.Column(sql.Integer, primary_key=True)
+    name = sql.Column(sql.String(50))
+    fullname = sql.Column(sql.String(50))
+    nickname = sql.Column(sql.String(50))
+
+    def __repr__(self):
+        return "<User(name='{}', fullname='{}', nickname='{}')>".format(
+            self.name, self.fullname, self.nickname
+        )
 
 
 def sqlalchemy_version():
@@ -19,6 +37,8 @@ def create_engine():
 def main():
     sqlalchemy_version()
     eng = create_engine()
+
+    print(repr(User.__table__))
 
 
 if __name__ == '__main__':
