46d1b4e1bc6d0666c60aa48525c6daf676e94c30
Pedro Guilherme Siqueira Moreira <pedro.guilherme@idvlab.com.br>
2019-08-26 16:41:06 -0300

    Build a many to many relationship

--- models.py
+++ models.py
@@ -33,3 +33,46 @@ class User(Base):
         return "<User(name='%s', fullname='%s', nickname='%s')>" % (
             self.name, self.fullname, self.nickname,
         )
+
+
+# association table
+post_keywords = sql.Table(
+    'post_keywords', Base.metadata,
+    sql.Column('post_id', sql.ForeignKey('posts.id'), primary_key=True),
+    sql.Column('keyword_id', sql.ForeignKey('keywords.id'), primary_key=True)
+)
+
+
+class BlogPost(Base):
+    __tablename__ = 'posts'
+
+    id = sql.Column(sql.Integer, primary_key=True)
+    user_id = sql.Column(sql.Integer, sql.ForeignKey('users.id'))
+    headline = sql.Column(sql.String(255), nullable=False)
+    body = sql.Column(sql.Text)
+
+    # many-to-many BlogPost<->Keyword
+    keywords = sql.orm.relationship('Keyword',
+                                    secondary=post_keywords,
+                                    back_populates='posts')
+
+    def __init__(self, headline, body, author):
+        self.author = author
+        self.headline = headline
+        self.body = body
+
+    def __repr__(self):
+        return "BlogPost(%r, %r, %r)" % (self.headline, self.body, self.author)
+
+
+class Keyword(Base):
+    __tablename__ = 'keywords'
+
+    id = sql.Column(sql.Integer, primary_key=True)
+    keyword = sql.Column(sql.String(50), nullable=False, unique=True)
+    posts = sql.orm.relationship('BlogPost',
+                                 secondary=post_keywords,
+                                 back_populates='keywords')
+
+    def __init__(self, keyword):
+        self.keyword = keyword
