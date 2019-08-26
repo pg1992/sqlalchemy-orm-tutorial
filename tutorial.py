92f6c377456ea8f87cb1f3bf911c2afb419bf85c
Pedro Guilherme Siqueira Moreira <pedro.guilherme@idvlab.com.br>
2019-08-26 15:31:26 -0300

    Move model objects to a separate module

--- tutorial.py
+++ tutorial.py
@@ -1,38 +1,5 @@
 import sqlalchemy as sql
-from sqlalchemy.ext.declarative import declarative_base
-
-
-Base = declarative_base()
-
-
-class Address(Base):
-    __tablename__ = 'addresses'
-
-    id = sql.Column(sql.Integer, primary_key=True)
-    email_address = sql.Column(sql.String, nullable=False)
-    user_id = sql.Column(sql.Integer, sql.ForeignKey('users.id'))
-
-    user = sql.orm.relationship('User', back_populates='addresses')
-
-    def __repr__(self):
-        return "Address(email_address='%s')" % self.email_address
-
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = sql.Column(sql.Integer, primary_key=True)
-    name = sql.Column(sql.String)
-    fullname = sql.Column(sql.String)
-    nickname = sql.Column(sql.String)
-
-    addresses = sql.orm.relationship('Address', back_populates='user',
-                                     cascade='all, delete, delete-orphan')
-
-    def __repr__(self):
-        return "<User(name='%s', fullname='%s', nickname='%s')>" % (
-            self.name, self.fullname, self.nickname,
-        )
+from models import Base, Address, User
 
 
 def create_engines_and_sessions():
